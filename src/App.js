d5c66212ca36a2f0b0e4b7416e718c548dcc672c
Lee cho i <cho.i.lee.1223@gmail.com>
2022-12-29 21:54:58 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
 						model: "text-davinci-003",
 						prompt: value,
 						temperature: 0.7,
-						max_tokens: 100,
+						max_tokens: 256,
 						top_p: 1,
 						frequency_penalty: 0,
 						presence_penalty: 0,
