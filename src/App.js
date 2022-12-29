fba488c25fef6b3158419c5b8da0f9430ec461f0
이초이 <choi1223@hunet.co.kr>
2022-12-29 15:41:05 +0900

    ing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import { useState } from 'react';
 import './App.scss';
 import { Configuration, OpenAIApi } from "openai";
-//import { Configuration, OpenAIApi } from 'https://cdn.skypack.dev/openai';
-
-//console.log(process.env.REACT_APP_API_KEY);
 
 function App() {
 	const [value, setValue] = useState('');
-	const [chat, setChat] = useState([]);
+	const [prompt, setPrompt] = useState([]);
+	const [res, setRes] = useState([]);
+
+	const [chatList, setChatList] = useState([]);
+
+	const configuration = new Configuration({
+		apiKey: process.env.REACT_APP_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
 
 	return (
 		<div className="App">
 			<div className="chat-content">
-				<div className="line">
-					<span className="chat-box">안녕?</span>
-				</div>
-				{/* <div className="line">
-					<span className="chat-box mine">안녕?</span>
-				</div> */}
-				{
-					chat.map((data, i) => (
+				{/* {
+					prompt.map((data, i) => (
 						<div className="line" key={i}>
 							<span className="chat-box mine">{data}</span>
 						</div>
 					))
 				}
+				{
+					res.map((data, i) => (
+						<div className="line" key={i}>
+							<span className="chat-box">{data}</span>
+						</div>
+					))
+				} */}
 			</div>
-
+			{/*
 			<form>
 				<input type="text" placeholder="입력하세요." value={value} onChange={(e) => {
 					setValue(e.target.value);
 				}} />
 				<button onClick={(e) => {
 					e.preventDefault();
-					const copy = [...chat];
+					const copy = [...prompt];
 					copy.push(value);
-					setChat(copy);
+					setPrompt(copy);
 					setValue('');
-
-
-					const configuration = new Configuration({
-						apiKey: process.env.REACT_APP_API_KEY,
-					});
-					const openai = new OpenAIApi(configuration);
 
 					openai.createCompletion({
 						model: "text-davinci-003",
-						prompt: { value },
+						prompt: value,
 						temperature: 0.7,
 						max_tokens: 100,
 						top_p: 1,
@@ -54,12 +54,38 @@ function App() {
 						presence_penalty: 0,
 					}).then((result) => {
 						console.log(result.data.choices[0].text);
+						const copy = [...res];
+						copy.push(result.data.choices[0].text);
+						setRes(copy);
 					});
 
 				}} disabled={!value}>전송</button>
+			</form> */}
+			{chatList}
+			<form>
+				<input type="text" placeholder="입력하세요." value={value} onChange={(e) => {
+					setValue(e.target.value);
+					const copy = [...prompt];
+					copy.push(value);
+					setPrompt(copy);
+					// setValue('');
+				}} />
+				<button onClick={(e) => {
+					e.preventDefault();
+					//setChatList(chatList.concat(<UserMsg prompt={prompt} />));
+
+				}}>전송</button>
 			</form>
 		</div>
 	);
+}
+
+function UserMsg({ prompt }) {
+	return (
+		<div className="line">
+			<span className="chat-box">{prompt}</span>
+		</div>
+	)
 }
 
 export default App;
